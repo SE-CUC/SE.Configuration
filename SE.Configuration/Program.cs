0e641c0044caddb423e961fbd5fb544558b7085f
k-edr <yevhenpuzenko@gmail.com>
2025-08-11 14:56:28 +0200

    Add project files.

---
+++ SE.Configuration/Program.cs
@@ -0,0 +1,17 @@
+using Sandbox.ModAPI.Ingame;
+using System;
+using VRage.Game.ModAPI.Ingame;
+
+namespace IngameScript
+{
+    public sealed partial class Program : MyGridProgram
+    {
+        public Program()
+        {
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+        }
+    }
+}
